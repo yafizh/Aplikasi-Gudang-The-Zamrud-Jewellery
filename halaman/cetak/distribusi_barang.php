6f90aa3854164efd5d59f2a3491d102fd5b9986e
A Chairil Yafizh <a.chairil.yafizh@gmail.com>
2023-01-30 08:27:35 +0700

    feature report and print report pendistribusian barang

---
+++ halaman/cetak/distribusi_barang.php
@@ -0,0 +1,144 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laporan Pendistribusian</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="shortcut icon" href="../../assets/img/logo2.png" />
+</head>
+
+<body>
+    <?php include_once('../../templates/header.php'); ?>
+    <h4 class="text-center my-3">Laporan Pendistribusian</h4>
+    <section class="px-3">
+        <?php if (isset($_POST['id_toko'])) : ?>
+            <?php $toko = $mysqli->query("SELECT * FROM toko WHERE id=" . $_POST['id_toko'])->fetch_assoc(); ?>
+        <?php endif; ?>
+        <?php if (isset($_POST['id_petugas'])) : ?>
+            <?php $petugas = $mysqli->query("SELECT * FROM petugas WHERE id=" . $_POST['id_petugas'])->fetch_assoc(); ?>
+        <?php endif; ?>
+        <?php if (isset($_POST['id_jenis_barang'])) : ?>
+            <?php $jenis_barang = $mysqli->query("SELECT * FROM jenis_barang WHERE id=" . $_POST['id_jenis_barang'])->fetch_assoc(); ?>
+        <?php endif; ?>
+        <div class="row">
+            <div class="col-12 col-sm-6">
+                <table class="table">
+                    <tr>
+                        <th colspan="2">Filter</th>
+                    </tr>
+                    <tr>
+                        <td class="align-middle td-fit">Nama Toko Yang Menerima</td>
+                        <td class="pl-5">: <?= $toko['nama'] ?? 'Semua Toko'; ?></td>
+                    </tr>
+                    <tr>
+                        <td class="align-middle td-fit">Petugas Yang Menerima</td>
+                        <td class="pl-5">: <?= $petugas['nama'] ?? 'Semua Petugas'; ?></td>
+                    </tr>
+                    <tr>
+                        <td class="align-middle td-fit">Jenis Barang</td>
+                        <td class="pl-5">: <?= $jenis_barang['nama'] ?? 'Semua Jenis Barang'; ?></td>
+                    </tr>
+                    <tr>
+                        <td class="align-middle td-fit">Tanggal</td>
+                        <?php if (isset($_POST['dari_tanggal']) && isset($_POST['sampai_tanggal'])) : ?>
+                            <td class="pl-5">: <?= indonesiaDate($_POST['dari_tanggal']); ?> - <?= indonesiaDate($_POST['sampai_tanggal']); ?></td>
+                        <?php else : ?>
+                            <td class="pl-5">: Semua Waktu</td>
+                        <?php endif; ?>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </section>
+    <main class="p-3">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th class="td-fit text-center align-middle">No</th>
+                    <th class="text-center align-middle">Tanggal</th>
+                    <th class="text-center align-middle">Nama Toko Yang Menerima</th>
+                    <th class="text-center align-middle">Petugas Yang Menerima</th>
+                    <th class="text-center align-middle">Jenis Barang</th>
+                    <th class="text-center align-middle">Kode Barang</th>
+                    <th class="text-center align-middle">Nama Barang</th>
+                    <th class="text-center align-middle">Jumlah</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $q = "
+                    SELECT 
+                        distribusi_barang.tanggal,
+                        toko.nama toko,
+                        petugas.nama petugas,
+                        jb.nama jenis_barang,
+                        jb.kode kode_jenis_barang,
+                        b.kode kode_barang,
+                        b.nama nama_barang,
+                        dp.jumlah 
+                    FROM 
+                        detail_distribusi_barang dp 
+                    INNER JOIN 
+                        distribusi_barang
+                    ON 
+                        distribusi_barang.id=dp.id_distribusi_barang 
+                    INNER JOIN 
+                        toko
+                    ON 
+                        toko.id=distribusi_barang.id_toko 
+                    INNER JOIN 
+                        petugas
+                    ON 
+                        petugas.id=distribusi_barang.id_petugas 
+                    INNER JOIN 
+                        barang b
+                    ON 
+                        b.id=dp.id_barang  
+                    INNER JOIN 
+                        jenis_barang jb 
+                    ON 
+                        jb.id=b.id_jenis_barang 
+                    WHERE 
+                        1=1 
+                ";
+
+                if (!empty($_POST['id_toko'] ?? ''))
+                    $q .= " AND distribusi_barang.id_toko=" . $_POST['id_toko'];
+                if (!empty($_POST['id_petugas'] ?? ''))
+                    $q .= " AND distribusi_barang.id_petugas=" . $_POST['id_petugas'];
+                if (!empty($_POST['id_jenis_barang'] ?? ''))
+                    $q .= " AND b.id_jenis_barang=" . $_POST['id_jenis_barang'];
+                if (!empty($_POST['dari_tanggal'] ?? '') && !empty($_POST['sampai_tanggal'] ?? ''))
+                    $q .= " AND (distribusi_barang.tanggal >='" . $_POST['dari_tanggal'] . "' AND distribusi_barang.tanggal <= '" . $_POST['dari_tanggal'] . "')";
+
+                $q .= " ORDER BY distribusi_barang.tanggal DESC, distribusi_barang.id DESC";
+                $result = $mysqli->query($q);
+                $no = 1;
+                ?>
+                <?php if ($result->num_rows) : ?>
+                    <?php while ($row = $result->fetch_assoc()) : ?>
+                        <tr>
+                            <td class="td-fit align-middle text-center"><?= $no++; ?></td>
+                            <td class="align-middle text-center"><?= indonesiaDate($row['tanggal']); ?></td>
+                            <td class="align-middle text-center"><?= $row['toko']; ?></td>
+                            <td class="align-middle"><?= $row['petugas']; ?></td>
+                            <td class="align-middle text-center"><?= $row['jenis_barang']; ?></td>
+                            <td class="align-middle text-center"><?= $row['kode_jenis_barang'] . generateKodeBarang($row['kode_barang']); ?></td>
+                            <td class="align-middle"><?= $row['nama_barang']; ?></td>
+                            <td class="align-middle text-center"><?= $row['jumlah']; ?></td>
+                        </tr>
+                    <?php endwhile; ?>
+                <?php else : ?>
+                    <tr>
+                        <td class="text-center" colspan="8">Tidak Ada Data</td>
+                    </tr>
+                <?php endif; ?>
+            </tbody>
+        </table>
+    </main>
+    <?php include_once('../../templates/footer.php'); ?>
+</body>
+
+</html>
---
+++ halaman/laporan/distribusi_barang.php
@@ -0,0 +1,175 @@
+<div class="container-fluid">
+
+    <div class="d-flex justify-content-between align-items-center">
+        <h1 class="h3 mb-0 text-gray-800">Laporan Pendistribusian</h1>
+    </div>
+
+    <hr>
+
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-8">
+            <div class="card shadow mb-4">
+                <form action="halaman/cetak/distribusi_barang.php" method="POST" target="_blank">
+                    <div class="card-header py-3 d-flex justify-content-between align-items-center">
+                        <h6 class="m-0 font-weight-bold text-primary">Filter</h6>
+                        <div>
+                            <?php if (!empty($_POST['id_jenis_barang'])) : ?>
+                                <input type="text" name="id_jenis_barang" hidden value="<?= $_POST['id_jenis_barang']; ?>">
+                            <?php endif; ?>
+                            <?php if (!empty($_POST['id_toko'])) : ?>
+                                <input type="text" name="id_toko" hidden value="<?= $_POST['id_toko']; ?>">
+                            <?php endif; ?>
+                            <?php if (!empty($_POST['id_petugas'])) : ?>
+                                <input type="text" name="id_petugas" hidden value="<?= $_POST['id_petugas']; ?>">
+                            <?php endif; ?>
+                            <?php if (!empty($_POST['dari_tanggal']) && !empty($_POST['sampai_tanggal'])) : ?>
+                                <input type="text" name="dari_tanggal" hidden value="<?= $_POST['dari_tanggal']; ?>">
+                                <input type="text" name="sampai_tanggal" hidden value="<?= $_POST['sampai_tanggal']; ?>">
+                            <?php endif; ?>
+                        </div>
+                        <button type="submit" class="btn btn-info">Cetak</button>
+                    </div>
+                </form>
+                <form action="" method="POST">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-12 col-md-6">
+                                <div class="mb-3">
+                                    <?php $toko = $mysqli->query("SELECT * FROM toko ORDER BY nama"); ?>
+                                    <label for="id_toko" class="form-label">Nama Toko Yang Menerima</label>
+                                    <select name="id_toko" id="id_toko" class="form-control">
+                                        <option value="" selected disabled>Semua Toko</option>
+                                        <?php while ($row = $toko->fetch_assoc()) : ?>
+                                            <option <?= $row['id'] == ($_POST['id_toko'] ?? '') ? 'selected' : ''; ?> value="<?= $row['id']; ?>"><?= $row['nama']; ?></option>
+                                        <?php endwhile; ?>
+                                    </select>
+                                </div>
+                                <div class="mb-3">
+                                    <?php $petugas = $mysqli->query("SELECT * FROM petugas ORDER BY nama"); ?>
+                                    <label for="id_petugas" class="form-label">Petugas Yang Menerima</label>
+                                    <select name="id_petugas" id="id_petugas" class="form-control">
+                                        <option value="" selected disabled>Semua Petugas</option>
+                                        <?php while ($row = $petugas->fetch_assoc()) : ?>
+                                            <option <?= $row['id'] == ($_POST['id_petugas'] ?? '') ? 'selected' : ''; ?> value="<?= $row['id']; ?>"><?= $row['nama']; ?></option>
+                                        <?php endwhile; ?>
+                                    </select>
+                                </div>
+                                <div class="mb-3">
+                                    <?php $jenis_barang = $mysqli->query("SELECT * FROM jenis_barang ORDER BY nama"); ?>
+                                    <label for="id_jenis_barang" class="form-label">Jenis Barang</label>
+                                    <select name="id_jenis_barang" id="id_jenis_barang" class="form-control">
+                                        <option value="" selected disabled>Semua Jenis Barang</option>
+                                        <?php while ($row = $jenis_barang->fetch_assoc()) : ?>
+                                            <option <?= $row['id'] == ($_POST['id_jenis_barang'] ?? '') ? 'selected' : ''; ?> value="<?= $row['id']; ?>"><?= $row['nama']; ?></option>
+                                        <?php endwhile; ?>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="mb-3">
+                                    <label for="dari_tanggal" class="form-label">Dari Tanggal</label>
+                                    <input type="date" class="form-control" name="dari_tanggal" id="dari_tanggal" value="<?= $_POST['dari_tanggal'] ?? ''; ?>">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="dari_tanggal" class="form-label">Sampai Tanggal</label>
+                                    <input type="date" class="form-control" name="sampai_tanggal" id="sampai_tanggal" value="<?= $_POST['sampai_tanggal'] ?? ''; ?>">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer d-flex justify-content-end" style="gap: .5rem;">
+                        <a href="" class="btn btn-secondary">Reset</a>
+                        <button class="btn-primary btn">Filter</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="card shadow mb-4">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered" id="reportTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th class="td-fit text-center">No</th>
+                                    <th class="text-center">Tanggal</th>
+                                    <th class="text-center">Nama Toko Yang Menerima</th>
+                                    <th class="text-center">Petugas Yang Menerima</th>
+                                    <th class="text-center">Jenis Barang</th>
+                                    <th class="text-center">Kode Barang</th>
+                                    <th class="text-center">Nama Barang</th>
+                                    <th class="text-center">Jumlah</th>
+                                </tr>
+                            </thead>
+                            <?php
+                            $q = "
+                                SELECT 
+                                    distribusi_barang.tanggal,
+                                    toko.nama toko,
+                                    petugas.nama petugas,
+                                    jb.nama jenis_barang,
+                                    jb.kode kode_jenis_barang,
+                                    b.kode kode_barang,
+                                    b.nama nama_barang,
+                                    dp.jumlah 
+                                FROM 
+                                    detail_distribusi_barang dp 
+                                INNER JOIN 
+                                    distribusi_barang
+                                ON 
+                                    distribusi_barang.id=dp.id_distribusi_barang 
+                                INNER JOIN 
+                                    toko
+                                ON 
+                                    toko.id=distribusi_barang.id_toko 
+                                INNER JOIN 
+                                    petugas
+                                ON 
+                                    petugas.id=distribusi_barang.id_petugas 
+                                INNER JOIN 
+                                    barang b
+                                ON 
+                                    b.id=dp.id_barang  
+                                INNER JOIN 
+                                    jenis_barang jb 
+                                ON 
+                                    jb.id=b.id_jenis_barang 
+                                WHERE 
+                                    1=1 
+                            ";
+
+                            if (!empty($_POST['id_toko'] ?? ''))
+                                $q .= " AND distribusi_barang.id_toko=" . $_POST['id_toko'];
+                            if (!empty($_POST['id_petugas'] ?? ''))
+                                $q .= " AND distribusi_barang.id_petugas=" . $_POST['id_petugas'];
+                            if (!empty($_POST['id_jenis_barang'] ?? ''))
+                                $q .= " AND b.id_jenis_barang=" . $_POST['id_jenis_barang'];
+                            if (!empty($_POST['dari_tanggal'] ?? '') && !empty($_POST['sampai_tanggal'] ?? ''))
+                                $q .= " AND (distribusi_barang.tanggal >='" . $_POST['dari_tanggal'] . "' AND distribusi_barang.tanggal <= '" . $_POST['dari_tanggal'] . "')";
+
+                            $q .= " ORDER BY distribusi_barang.tanggal DESC, distribusi_barang.id DESC";
+                            $result = $mysqli->query($q);
+                            $no = 1;
+                            ?>
+                            <tbody>
+                                <?php while ($row = $result->fetch_assoc()) : ?>
+                                    <tr>
+                                        <td class="td-fit align-middle text-center"><?= $no++; ?></td>
+                                        <td class="align-middle text-center"><?= indonesiaDate($row['tanggal']); ?></td>
+                                        <td class="align-middle text-center"><?= $row['toko']; ?></td>
+                                        <td class="align-middle"><?= $row['petugas']; ?></td>
+                                        <td class="align-middle text-center"><?= $row['jenis_barang']; ?></td>
+                                        <td class="align-middle text-center"><?= $row['kode_jenis_barang'] . generateKodeBarang($row['kode_barang']); ?></td>
+                                        <td class="align-middle"><?= $row['nama_barang']; ?></td>
+                                        <td class="align-middle text-center"><?= $row['jumlah']; ?></td>
+                                    </tr>
+                                <?php endwhile; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
